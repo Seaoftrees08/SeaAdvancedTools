ced386e68fc69a59c0a6637fd06d530d4cad1452
Seaoftrees08 <tasuzuya@gmail.com>
2023-02-06 18:49:46 +0900

    create enhancer

--- src/main/java/com/github/seaoftrees08/seaadvancedtools/item/EntoolItem.java
+++ src/main/java/com/github/seaoftrees08/seaadvancedtools/item/EntoolItem.java
@@ -2,7 +2,6 @@ package com.github.seaoftrees08.seaadvancedtools.item;
 
 import com.github.seaoftrees08.seaadvancedtools.EnhancedTools;
 import com.github.seaoftrees08.seaadvancedtools.EntoolCreativeModeTab;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -15,6 +14,21 @@ public class EntoolItem {
 
     public static final RegistryObject<Item> TEST = ITEMS.register(
             "test",
+            () -> new Item(new Item.Properties().tab(EntoolCreativeModeTab.ENTOOL_TAB))
+    );
+
+    public static final RegistryObject<Item> HIGH_ENHANCER = ITEMS.register(
+            "high_enhancer",
+            () -> new Item(new Item.Properties().tab(EntoolCreativeModeTab.ENTOOL_TAB))
+    );
+
+    public static final RegistryObject<Item> MIDDLE_ENHANCER = ITEMS.register(
+            "middle_enhancer",
+            () -> new Item(new Item.Properties().tab(EntoolCreativeModeTab.ENTOOL_TAB))
+    );
+
+    public static final RegistryObject<Item> LOW_ENHANCER = ITEMS.register(
+            "low_enhancer",
             () -> new Item(new Item.Properties().tab(EntoolCreativeModeTab.ENTOOL_TAB))
     );
 
